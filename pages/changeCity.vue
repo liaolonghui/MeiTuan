a2369d0a5518626fb166c2db6de323c876b56dd3
liaolonghui <1340881930@qq.com>
2020-09-27 18:38:11 +0800

    城市切换

---
+++ pages/changeCity.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page-changeCity">
+        <el-row>
+            <el-col :span="24">
+                <iSelect/>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <hot/>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <categroy/>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import iSelect from '@/components/changeCity/iselect.vue'
+import hot from '@/components/changeCity/hot.vue'
+import categroy from '@/components/changeCity/categroy.vue'
+
+export default {
+    components: {
+        iSelect,
+        hot,
+        categroy
+    }
+}
+</script>
